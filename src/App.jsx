92d1c40d43f7dd514bfbd29bdaa43832fe6d9f8d
dimusz <dina.lucian@yahoo.com>
2023-02-06 10:24:41 +0200

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <Route path="/despre" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
+  <Footer />
     </div>
   );
 }
